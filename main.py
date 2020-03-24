66c883e18d5bf14abae64489b62e49a0dfe8786d
EdinsonRequena <edinsonrequena@protonmail.com>
2020-03-23 22:39:00 -0400

    adding ball animation

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 class PongBall(Widget):
 
@@ -22,13 +26,32 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        
+    
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if self.ball.y < 0 or self.ball.top > self.height:
+            self.ball.velocity_y *= -1
+        
+        # bounce off left and right
+        if self.ball.x < 0 or self.ball.right > self.width:
+            self.ball.velocity_x *= -1
 
 
 class PongApp(App):
     
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
